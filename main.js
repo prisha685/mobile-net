c948575ae7f7f892e872ea00cc6a55a3f3ba5929
prisha685 <77448105+prisha685@users.noreply.github.com>
2021-06-12 19:54:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,27 @@ Webcam.set ({
 });
 camera=document.getElementById("camera");
 Webcam.attach('#camera');
+function take_snapshot(){
+  Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+  });
+}
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+function modelLoaded() {
+  console.log('model Loaded');
+}
+function check() {
+img=document.getElementById('captured_image');
+classifier.classifiy(img,gotResult);
+}
+function gotResult(error,results) {
+if(error){
+  comsole.error(error);
 
+}
+else{
+  console.log(results);
+  document.getElementById("object_name").innerHTML=results[0].label;
+}
+}
